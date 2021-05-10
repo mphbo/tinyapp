747c9bd971aecd27da1f77e7bbe1c4a071816512
Logan Thomas <logannormanthomas@protonmail.com>
2021-05-10 22:20:29 +0000

    added a generateRandomString and post

--- express_server.js
+++ express_server.js
@@ -17,6 +17,8 @@ app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
 
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get('/urls.json', (req, res) => {
   res.json(urlDatabase);
@@ -31,7 +33,20 @@ app.get('/urls', (req, res) => {
   res.render('urls_index', templateVars);
 });
 
+app.get('/urls/new', (req, res) => {
+  res.render('urls_new');
+});
+
 app.get('/urls/:shortURL', (req, res) => {
   const templateVars = {shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
   res.render('urls_show', templateVars);
 });
+
+app.post('/urls', (req, res) => {
+  console.log(req.body);
+  res.send('Ok');
+});
+
+const generateRandomString = () => {
+  const string = Math.random().toString(16).substr(2, 6);
+}
